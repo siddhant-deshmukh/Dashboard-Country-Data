e23bec14137fcf1507cfc5d20e6f45b751ba804a
siddhant-deshmukh <siddhant946044@gmail.com>
2023-12-29 00:11:11 +0530

    almost done! Added g20 redy for deployment!

--- data/world-data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [IF NOT EXISTS] countries (
-  country_code INT,
-  iso_code VARCHAR(3,3),
-  country VARCHAR(50),
-  subject_code VARCHAR(20),
-  subject_descriptor VARCHAR(90),
-  subject_notes VARCHAR(),
-);
